a19c3246d2c023938b28c454fd3a04435f4880da
luctst <lucas.tostee@gmail.com>
2020-06-21 18:58:04 +0200

    Handle login aoi logic

--- api/index.js
+++ api/index.js
@@ -1,22 +1,19 @@
 const express = require("express");
+
+const parseBody = require("./utils/parseBody");
 const responseServer = require("./utils/responseServer");
 const mongo = require("./db/index");
-const {hash} = require("bcrypt");
+const {hash, compareSync} = require("bcrypt");
 const {randomFillSync} = require("crypto");
 const {sign} = require("jsonwebtoken");
 
 const router = express.Router();
+
 router.use(express.json());
+router.use("/signup", parseBody);
+router.use("/login", parseBody);
 
 router.post("/signup", async function (req, res) {
-    const bodyArray = Object.keys(req.body);
-
-    if (bodyArray.length === 0) {
-        return responseServer(res, 400, {
-            content: "Missing content-type request header."
-        })
-    }
-
     if (bodyArray.length > 2) {
         return responseServer(res, 400);
     }
@@ -47,4 +44,44 @@ router.post("/signup", async function (req, res) {
     return responseServer(res, 409);
 });
 
+router.post("/login", async function (req, res) {
+    if (bodyArray.length > 2) {
+        return responseServer(res, 400);
+    }
+
+    if (!bodyArray.includes("pseudo") || !bodyArray.includes("pswd")) {
+        return responseServer(res, 400);
+    }
+
+    const mClient = await mongo();
+    const usersCol = mClient.db().collection("users");
+
+    const user = await usersCol.findOne({pseudo: req.body.pseudo});
+
+    if (user === null) {
+        return responseServer(res, 403);
+    }
+
+    if (!compareSync(req.body.pswd, user.pswd)) {
+        return responseServer(res, 401)
+    };
+
+    return responseServer(res, 200, {
+        modifyResponse: {
+            token: sign({iss: user._id}, user.key)
+        }
+    })
+});
+
+
+router.get("/:id", function (req, res) {
+    if (!req.headers.authorization) {
+        return responseServer(res, 401);
+    }
+
+    const [b, token] = req.headers.authorization.split(" ");
+    if (b !== "Bearer" || !token || token.split(".").length !== 3) {
+        return responseServer(res, 400);
+    }
+});
 module.exports = router;
