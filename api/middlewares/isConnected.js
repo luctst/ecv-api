5b58ef47011c63b48a7c05c1d0d8bacca3d6a7bf
luctst <lucas.tostee@gmail.com>
2020-06-21 17:04:34 +0200

    Create isConnected middleware

---
+++ api/middlewares/isConnected.js
@@ -0,0 +1,9 @@
+const responseServer = require("../utils/responseServer");
+const mongo = require("../db/index");
+
+module.exports = async function isConnected (req, res, next) {
+    console.log(req.headers);
+
+    if (req.headers.)
+    next();
+};
