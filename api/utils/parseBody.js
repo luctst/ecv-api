2c457eb1f939431e241da80eb8bc02b6b9c4ba76
luctst <lucas.tostee@gmail.com>
2020-06-21 18:58:12 +0200

    Create parsebody liddleware

---
+++ api/utils/parseBody.js
@@ -0,0 +1,14 @@
+const responseServer = require("./responseServer");
+
+module.exports = function (req, res, next) {
+    const bodyArray = Object.keys(req.body);
+
+    if (bodyArray.length === 0) {
+        return responseServer(res, 400, {
+            content: "Missing content-type request header."
+        })
+    }
+
+    req.bodyArray = bodyArray;
+    next();
+}
