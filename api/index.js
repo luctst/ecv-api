6a6718e2675e0e8f74af63222f4ec46e3b6b4f69
luctst <lucas.tostee@gmail.com>
2020-06-21 17:04:07 +0200

    Delete comment

--- api/index.js
+++ api/index.js
@@ -2,9 +2,6 @@ const express = require("express");
 const router = express.Router();
 const signUpFn = require("./routes/signup");
 
-// router.use();
-
-
 router.get("/signup", function (req, res) {
     const r = signUpFn(req, res);
 
